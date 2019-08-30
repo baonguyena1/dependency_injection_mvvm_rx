a1c99808fd7c07396f8d33eb30a54c2c320f22d5
Bao Nguyen <bao.nguyen01@axonactive.com>
2019-08-30 16:31:18 +0700

    Add swiftlint

--- DependencyMVVM/Models/Company.swift
+++ DependencyMVVM/Models/Company.swift
@@ -4,19 +4,4 @@ class Company: Codable {
 	let name: String?
 	let catchPhrase: String?
 	let bs: String?
-
-	enum CodingKeys: String, CodingKey {
-
-		case name = "name"
-		case catchPhrase = "catchPhrase"
-		case bs = "bs"
-	}
-
-    required init(from decoder: Decoder) throws {
-		let values = try decoder.container(keyedBy: CodingKeys.self)
-		name = try values.decodeIfPresent(String.self, forKey: .name)
-		catchPhrase = try values.decodeIfPresent(String.self, forKey: .catchPhrase)
-		bs = try values.decodeIfPresent(String.self, forKey: .bs)
-	}
-
 }
